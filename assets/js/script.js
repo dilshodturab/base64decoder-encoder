e5ae686ed1e331fc6eb5adc523401f549811faa3
dilshodturabov <avlomberdioglid@gmail.com>
2024-02-27 12:44:59 +0500

    fully functional

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Making accessible these elements from HTML to JavaScript
+const textInput = document.getElementById('text-input');
+const outputText = document.getElementById('output-text');
+const encodeButton = document.getElementById('encode-button');
+const decodeButton = document.getElementById('decode-button');
+
+
+
+encodeButton.addEventListener('click', () => {
+    const encodedText = btoa(textInput.value);
+    outputText.value = encodedText;
+});
+
+decodeButton.addEventListener('click', () => {
+    try {
+        const decodedText = atob(textInput.value);
+        outputText.value = decodedText;
+    } catch (e) {
+        alert(`${textInput.value} is not decodable! `);
+        console.error(e);
+    }
+})
